82c101dc6d15f4d7e44fe6753e054d3153e029a1
Aloisli Bürgisser <Aloisli Brgisser>
2016-05-20 13:45:57 +0200

    Start db-configuration
      + Migrations are creatable but doesn't create anything

---
+++ src/DownieDrive/DownieDrive.Businesslogic/Migrations/201605201143261_Init.cs
@@ -0,0 +1,25 @@
+namespace DownieDrive.Businesslogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.File",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.File");
+        }
+    }
+}
---
+++ src/DownieDrive/DownieDrive.Businesslogic/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace DownieDrive.Businesslogic.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DownieDrive.Businesslogic.DownieContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DownieDrive.Businesslogic.DownieContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
