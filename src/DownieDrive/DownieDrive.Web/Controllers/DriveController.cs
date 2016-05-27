26a33c42175ef5c37bc4e5ef642f242db3435374
BrunnerLivio <livio.brunner@hotmail.com>
2016-05-27 13:24:26 +0200

    add drivelayout structure

---
+++ src/DownieDrive/DownieDrive.Web/Controllers/DriveController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DownieDrive.Web.Controllers
+{
+    /// <summary>
+    /// Representiert den Drive Controller.
+    /// In diesen Controller, gelangt man nur wenn man Eingeloggt ist. 
+    /// Hier werden Dateien, Ordner erstellen, gelöscht & verschoben.
+    /// </summary>
+    public class DriveController : Controller
+    {
+        /// <summary>
+        /// Gibt die Index View zurück, welche eine Übersicht aller Dateien
+        /// des Users darstellt
+        /// </summary>
+        /// <returns>Die Index View</returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return PartialView("~/Views/Shared/DriveMenu.cshtml");
+        }
+    }
+}
